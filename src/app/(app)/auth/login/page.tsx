4e402a13c46d43b138acb764f3bb7fc9cd600359
MUHAMMAD Numan <numanimran661@gmail.com>
2025-01-19 00:22:05 +0500

    eslint uninstalled

--- src/app/(app)/auth/login/page.tsx
+++ src/app/(app)/auth/login/page.tsx
@@ -1,20 +1,22 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
-import InputField from '@/app/components/common/inputFields/InputField';
-import Button from '@/app/components/common/buttons/Button';
-import { GoogleLogo } from '@/app/components/common/allImages/AllImages';
-import Link from 'next/link';
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import InputField from "@/app/components/common/inputFields/InputField";
+import Button from "@/app/components/common/buttons/Button";
+import { GoogleLogo } from "@/app/components/common/allImages/AllImages";
+import Link from "next/link";
 
 const LoginPage: React.FC = () => {
   return (
     <Formik
-      initialValues={{ email: '', password: '' }}
+      initialValues={{ email: "", password: "" }}
       validationSchema={Yup.object({
-        email: Yup.string().email('Invalid email address').required('Required'),
-        password: Yup.string().min(6, 'Password must be at least 6 characters').required('Required'),
+        email: Yup.string().email("Invalid email address").required("Required"),
+        password: Yup.string()
+          .min(6, "Password must be at least 6 characters")
+          .required("Required"),
       })}
       onSubmit={(values, { setSubmitting }) => {
         console.log(values);
@@ -23,28 +25,40 @@ const LoginPage: React.FC = () => {
     >
       {({ isSubmitting }) => (
         <Form>
-          <h2 className='text-3xl font-bold mb-7'>Log in to your account</h2>
-          <InputField label="Email Address" name="email" type="email" placeholder='Email Address' />
-          <InputField label="Password" name="password" type="password" placeholder='Password' />
-          <div className='flex justify-end mt-3'>
-            <p className='text-primary font-regular'>Forgot Password?</p>
+          <h2 className="text-3xl font-bold mb-7">Log in to your account</h2>
+          <InputField
+            label="Email Address"
+            name="email"
+            type="email"
+            placeholder="Email Address"
+          />
+          <InputField
+            label="Password"
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <div className="flex justify-end mt-3">
+            <p className="text-primary font-regular">Forgot Password?</p>
           </div>
           <Button
             type="submit"
             disabled={isSubmitting}
-            label='Log in'
-            className='mt-4 w-full'
+            label="Log in"
+            className="mt-4 w-full"
           />
           <Button
             type="button"
-            label='Log in with google'
+            label="Log in with google"
             icon={GoogleLogo}
-            className='mt-4 w-full bg-lightBlue'
-            variant='light'
+            className="mt-4 w-full bg-lightBlue"
+            variant="light"
           />
-          <div className='mt-4 font-regular'>
+          <div className="mt-4 font-regular">
             <span>Don't have an account?</span>{" "}
-            <Link href={'/auth/signup'} className='text-primary'>Sign up</Link>
+            <Link href={"/auth/signup"} className="text-primary">
+              Sign up
+            </Link>
           </div>
         </Form>
       )}
